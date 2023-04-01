8f6e737d1f1919d89215b6cbc9616ba660010e63
kamal <kamalkumar114@gmail.com>
2023-04-01 11:45:55 +0530

    ProjectKK

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('ejs', ejsMate);
 
-const dburl = process.env.DB_URL || 'mongodb://localhost:27017/ProjectKK'; 
+const dburl = process.env.DB_URL ; 
 mongoose.set('strictQuery', true);
 mongoose.connect(dburl, {
     useNewUrlParser: true,
