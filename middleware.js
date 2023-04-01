ed989cce28b09cf2baab7c40c2351a095a59d591
Kamal Kumar <125093696+gkamal-kumar@users.noreply.github.com>
2023-04-01 18:54:19 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ const isLoggedin = function(req, res, next) {
     const passport = req.session.passport;
     if (!passport) {
         req.flash('error', 'you must be signed in');
-        res.redirect('/login'); 
+        return res.redirect('/login'); 
     }
     return next();
 } 
